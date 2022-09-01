c77853d7e1a3a2c2ef03ccd3a8e38660608b5f24
Romina2219 <104035349+Romina2219@users.noreply.github.com>
2022-09-01 08:30:42 -0300

    Add files via upload

---
+++ tp1_backend.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<h1>Trabajo Práctico N°1</h1>
+
+<?php
+echo "Hola Mundo";
+
+ ?>
+<br>
+<hr>
+<?php
+$mensaje = "Hola Mundo";
+echo $mensaje;
+
+ ?>
+<br>
+<hr>
+<?php
+$campera = 150;
+$jean = 20;
+
+echo $campera + $jean;
+
+ ?>
+<br>
+<hr>
+
+<?php
+echo $campera - $jean;
+
+ ?>
+ <br>
+ <hr>
+
+ <?php
+echo $campera * $jean;
+  ?>
+ <br>
+ <hr>
+
+ <?php
+ echo $campera / $jean;
+  ?>
+ <br>
+ <hr>
+
+ <?php
+echo $campera % $jean;
+  ?>
+  <br>
+  <hr>
+
+  <?php
+$celsius = 20;
+$farhren = 1.8;
+echo $celsius * $farhren + 32;
+
+   ?>
+   <br>
+   <hr>
+
+   <?php
+$base = 18;
+$altura = 12;
+echo $base *2 + $altura *2;
+
+    ?>
+    <br>
+    <hr>
+
+    <?php
+$radio = 30;
+$pi = 3.1416;
+echo $radio *2 * $pi;
+
+     ?>
+<br>
+<hr>
+
+     <?php
+echo $radio **2 * $pi;
+
+      ?>
+  </body>
+</html>
